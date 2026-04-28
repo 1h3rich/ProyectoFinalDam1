2d1b1bc8002c9c77e03288c0ac0b6c80cb81d985
isard <isard@DESKTOP-P0UA02U>
2026-04-28 17:14:59 +0200

    Cambios menores

--- ProyectoFinalDam_1/src/main/java/excepciones/Excepciones.java
+++ ProyectoFinalDam_1/src/main/java/excepciones/Excepciones.java
@@ -9,5 +9,6 @@ package excepciones;
  * @author isard
  */
 public class Excepciones {
+    // Esta excepcione no sirve unicamente esta para contener una clase en este paquete, hay que hacer cada clase con el nombre de cada excepcion
     
 }
