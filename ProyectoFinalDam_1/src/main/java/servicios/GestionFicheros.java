8297e2d076d97e3df6ef5ce0e0ac5ae6488bbe7e
Ricardo <rykhjob@gmail.com>
2026-04-29 18:15:26 +0200

    Se han agregado constantes, asi como la conexion SQL, usuario y contraseña, algunas adiciones de clases y se ha iniciado la gestion de ficheros.

--- ProyectoFinalDam_1/src/main/java/servicios/GestionFicheros.java
+++ ProyectoFinalDam_1/src/main/java/servicios/GestionFicheros.java
@@ -4,10 +4,30 @@
  */
 package servicios;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author isard
  */
 public class GestionFicheros {
     // En esta clase se va a encontrar todos los metodos necesarios para la gestion de ficheros (csv, txt, json, etc)
+    
+    /**
+     * Crea el fichero; se sabe que tipo de fichero es porque por parámetro le pasamos la ruta.
+     * @param ruta 
+     */
+    public static void crearFichero(String ruta){
+        File f = new File(ruta);
+        try {
+            f.createNewFile();
+        } catch (IOException ex) {
+            Logger.getLogger(GestionFicheros.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    
 }
