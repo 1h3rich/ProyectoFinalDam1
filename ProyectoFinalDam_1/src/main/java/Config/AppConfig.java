c6f0191d875e1b4d62179e59fe94af1fa330860b
isard <isard@DESKTOP-P0UA02U>
2026-05-07 18:49:27 +0200

    Appconfig

---
+++ ProyectoFinalDam_1/src/main/java/Config/AppConfig.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Config;
+
+/**
+ *
+ * @author isard
+ */
+public class AppConfig {
+     // SQL
+    public static String urlSQL = "jdbc:mysql://127.0.0.1:3306/centro_formacion";
+    public static String nombreUsuarioSQL = "root";
+    public static String[] contraseñaSQL = {"alumno", "root"};
+    
+    // Ficheros
+    
+    public static String ficheroAlumno = "alumno";
+    public static String ficheroCiclo = "ciclo";
+    public static String ficheroModulo = "modulo";
+    public static String ficheroLineaMatricula = "linea_matricula";
+    public static String ficheroMatricula = "matricula";
+}
