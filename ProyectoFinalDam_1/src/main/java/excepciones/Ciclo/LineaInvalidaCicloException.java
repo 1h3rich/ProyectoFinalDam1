e91ba516b04949b2e77f312bff64dc05c12e8c17
isard <isard@DESKTOP-P0UA02U>
2026-05-20 16:38:46 +0200

    Excepcion Linea Invalida Ciclo

---
+++ ProyectoFinalDam_1/src/main/java/excepciones/Ciclo/LineaInvalidaCicloException.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones.Ciclo;
+
+/**
+ *
+ * @author isard
+ */
+public class LineaInvalidaCicloException extends Exception{
+
+    /** Construye la excepción sin mensaje de detalle. */
+    public LineaInvalidaCicloException() {
+    }
+
+    /**
+     * Construye la excepción con un mensaje descriptivo del error.
+     *
+     * @param message Descripción del error.
+     */
+    public LineaInvalidaCicloException(String message) {
+        super(message);
+    }
+    
+}
