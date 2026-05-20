cc2a50cca36509b0be8223ddc1dff31c46ad9e28
isard <isard@DESKTOP-P0UA02U>
2026-05-20 16:17:51 +0200

    Excepcion Denominacion vacia

---
+++ ProyectoFinalDam_1/src/main/java/excepciones/Ciclo/DenominacionVaciaException.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones.Ciclo;
+
+/**
+ *
+ * @author isard
+ */
+public class DenominacionVaciaException extends Exception{
+
+    /** Construye la excepción sin mensaje de detalle. */
+    public DenominacionVaciaException() {
+    }
+
+     /**
+     * Construye la excepción con un mensaje descriptivo del error.
+     *
+     * @param message Descripción del error.
+     */
+    public DenominacionVaciaException(String message) {
+        super(message);
+    }
+    
+    
+}
