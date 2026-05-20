6aa4c571fcdc972d97a577417175a8ef6b1b6640
isard <isard@DESKTOP-P0UA02U>
2026-05-20 10:54:46 +0200

    Excepcion Linea Invalida Alumno

---
+++ ProyectoFinalDam_1/src/main/java/excepciones/Alumno/LineaInvalidaAlumnoException.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package excepciones.Alumno;
+
+/**
+ *
+ * @author isard
+ */
+public class LineaInvalidaAlumnoException extends Exception{
+
+     /** Construye la excepción sin mensaje de detalle. */
+    public LineaInvalidaAlumnoException() {
+    }
+
+    /**
+     * Construye la excepción con un mensaje descriptivo del error.
+     *
+     * @param message Descripción del error.
+     */
+    public LineaInvalidaAlumnoException(String message) {
+        super(message);
+    }
+    
+    
+}
